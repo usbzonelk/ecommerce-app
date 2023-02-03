df8eeeb667e8a99cb1fb21d0709fe1972ad9073f
chamith <erandachamith322@gmail.com>
2023-02-03 19:37:51 +0530

    implement end point to save user data to database

--- backend/src/main/java/com/example/backend/controller/UserController.java
+++ backend/src/main/java/com/example/backend/controller/UserController.java
@@ -1,4 +1,33 @@
 package com.example.backend.controller;
 
+import com.example.backend.DTO.RequestDTO.UserRegRequestDTO;
+import com.example.backend.service.UserService;
+import com.example.backend.util.StandardResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping(path = "api/v1/user")
+
 public class UserController {
+    @Autowired
+    private UserService userService;
+    @PutMapping(path = "/regUser")
+    public ResponseEntity<StandardResponse> saveUser(@RequestBody UserRegRequestDTO userRegRequestDTO){
+        String text = userService.regUser(userRegRequestDTO);
+        return new ResponseEntity<StandardResponse>(
+                new StandardResponse
+                        (
+                                201,
+                                "Saved successfully !!",
+                                text
+                        ), HttpStatus.CREATED);
+    }
+
+
+
 }
+
