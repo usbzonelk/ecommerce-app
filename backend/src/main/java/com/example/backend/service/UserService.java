1ade850dad103613b81d7db2ced1b60cfd3321f8
chamith <erandachamith322@gmail.com>
2023-02-03 19:47:11 +0530

    crete method signature

--- backend/src/main/java/com/example/backend/service/UserService.java
+++ backend/src/main/java/com/example/backend/service/UserService.java
@@ -1,4 +1,7 @@
 package com.example.backend.service;
 
+import com.example.backend.DTO.RequestDTO.UserRegRequestDTO;
+
 public interface UserService {
+    String regUser(UserRegRequestDTO userRegRequestDTO);
 }
