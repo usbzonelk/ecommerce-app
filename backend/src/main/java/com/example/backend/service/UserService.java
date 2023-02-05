eff2923d142fbbac47cf228cd86fa32845d18afa
chamith <erandachamith322@gmail.com>
2023-02-05 19:29:44 +0530

    Implement method logic to delete user by connecting user repository

--- backend/src/main/java/com/example/backend/service/UserService.java
+++ backend/src/main/java/com/example/backend/service/UserService.java
@@ -1,7 +1,12 @@
 package com.example.backend.service;
 
 import com.example.backend.DTO.RequestDTO.UserRegRequestDTO;
+import com.example.backend.DTO.ResponseDTO.UserResponseDTO;
+import com.example.backend.DTO.UserDTO;
+import javassist.NotFoundException;
 
 public interface UserService {
     String regUser(UserRegRequestDTO userRegRequestDTO);
+
+    UserResponseDTO getUserUsingID(int id) throws NotFoundException;
 }
