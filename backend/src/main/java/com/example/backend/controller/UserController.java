7b208453a2fb221b9838c6d33c60ffae54e29c64
chamith <erandachamith322@gmail.com>
2023-02-05 19:26:11 +0530

    create method to get user details by using ID- ID get using path variable

--- backend/src/main/java/com/example/backend/controller/UserController.java
+++ backend/src/main/java/com/example/backend/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.example.backend.controller;
 
 import com.example.backend.DTO.RequestDTO.UserRegRequestDTO;
+import com.example.backend.DTO.ResponseDTO.UserResponseDTO;
+import com.example.backend.DTO.UserDTO;
 import com.example.backend.service.UserService;
 import com.example.backend.util.StandardResponse;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,6 +28,18 @@ public class UserController {
                                 "Saved successfully !!",
                                 text
                         ), HttpStatus.CREATED);
+    }
+
+    @GetMapping(path = "/getUserID/{id}")
+    public ResponseEntity<StandardResponse> getUserByID(@PathVariable (value = "id") int id) throws NotFoundException {
+        UserResponseDTO userResponseDTO = userService.getUserUsingID(id);
+        return new ResponseEntity<StandardResponse>(
+                new StandardResponse
+                        (
+                                200,
+                                "This is the customer id = " + id,
+                                userResponseDTO
+                        ), HttpStatus.OK);
     }
 
 
