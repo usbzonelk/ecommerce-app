f131b0317e59fd7b7e4ae00727ee49853bb92a08
chamith <erandachamith322@gmail.com>
2023-02-05 19:40:21 +0530

    Add mehod signature of getUserUsingID()

--- backend/src/main/java/com/example/backend/service/impl/UserServiceIMPL.java
+++ backend/src/main/java/com/example/backend/service/impl/UserServiceIMPL.java
@@ -1,10 +1,13 @@
 package com.example.backend.service.impl;
 
 import com.example.backend.DTO.RequestDTO.UserRegRequestDTO;
+import com.example.backend.DTO.ResponseDTO.UserResponseDTO;
+import com.example.backend.DTO.UserDTO;
 import com.example.backend.entity.User;
 import com.example.backend.repo.UserRepo;
 import com.example.backend.service.UserService;
 import com.example.backend.util.StandardResponse;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -30,5 +33,20 @@ public class UserServiceIMPL implements UserService {
             return user.getCustomerName()+" is already registered " ;
         }
     }
-}
 
+    @Override
+    public UserResponseDTO getUserUsingID(int id) throws NotFoundException {
+        if(userRepo.existsById(id)){
+            User user = userRepo.getById(id);
+            UserResponseDTO userResponseDTO = new UserResponseDTO(
+                    user.getCustomerId(),
+                    user.getCustomerName(),
+                    user.getCustomerSalarry(),
+                    user.getContactNumber()
+            );
+            return userResponseDTO ;
+        }else{
+            throw new NotFoundException("User is not found id = " + "id");
+        }
+    }
+}
