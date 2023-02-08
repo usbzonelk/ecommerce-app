4f3bf7661ef33145d721a26a48135f18152e1045
Santhos <santhoshsunthar@gmail.com>
2023-02-08 14:31:51 +0530

    Added #NavBar section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import NavBar from './components/NavBar';
 
 const App = () => {
   return (
     <div className="App">
-      Hello!
+      <NavBar/>
     </div>
   );
 }
