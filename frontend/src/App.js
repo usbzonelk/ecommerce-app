fdaca9d1598abe1eea4484801d49eb66d9c053b1
Santhos <santhoshsunthar@gmail.com>
2023-02-08 15:38:38 +0530

    Added #SignUp section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from './components/NavBar';
+import SignUp from './components/SignUp'
 
 const App = () => {
   return (
     <div className="App">
       <NavBar/>
+      <SignUp/>
     </div>
   );
 }
