2ec4e598f0322200dd4a0a59689a6d5d98689bee
Santhos <santhoshsunthar@gmail.com>
2023-02-18 17:48:00 +0530

    Added #header-top section

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,69 @@
-import React from 'react';
-
+import React from 'react'
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
+import {IoPersonOutline, IoHeartOutline, IoBagHandleOutline} from 'react-icons/io5';
 const HomePage = () => {
 
   return (
     <div className="home-page">
-      <h1>Welcome to our store!</h1>
+      <header>
+        <div className="header-top">
+          <div className="container">
+            <ul class="header-social-container">
+
+              <li>
+                <a href="#" class="social-link">
+                  <FaFacebook />
+                </a>
+              </li>
+
+              <li>
+                <a href="#" class="social-link">
+                  <FaTwitter />
+                </a>
+              </li>
+
+              <li>
+                <a href="#" class="social-link">
+                  <FaInstagram />
+                </a>
+              </li>
+
+              <li>
+                <a href="#" class="social-link">
+                  <FaLinkedinIn />
+                </a>
+              </li>
+
+            </ul>
+
+            <div class="header-alert-news">
+              <p>
+                <b>Latest </b>
+                Laptop Models Now Available! 
+              </p>
+            </div>
+
+            <div class="header-user-actions">
+
+              <button class="action-btn">
+                <IoPersonOutline/>
+              </button>
+
+              <button class="action-btn">
+                <IoHeartOutline/>
+                <span class="count">0</span>
+              </button>
+
+              <button class="action-btn">
+                <IoBagHandleOutline/>
+                <span class="count">0</span>
+              </button>
+
+            </div>
+
+          </div>
+        </div>
+      </header>
     </div>
   );
 }
