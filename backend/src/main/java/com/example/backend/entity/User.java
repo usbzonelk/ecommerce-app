75de78ba9d51c3814cb2a799efcb643901b33149
chamith <erandachamith322@gmail.com>
2023-03-02 20:55:09 +0530

    do some modification on User entity

--- backend/src/main/java/com/example/backend/entity/User.java
+++ backend/src/main/java/com/example/backend/entity/User.java
@@ -15,29 +15,40 @@ import java.util.ArrayList;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "customer")
+@Table(name = "user")
 @TypeDefs({
         @TypeDef(name = "json",typeClass = JsonType.class)
 })
 public class User {
     @Id
-    @Column(name = "customer_id", length = 40)
+    @Column(name = "user_id", length = 40)
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int customerId;
 
 
-    @Column(name = "customer_name", length = 56)
+    @Column(name = "user_name", length = 56)
     private String customerName;
 
-    @Column(name = "customer_salary", length = 10, nullable = false)
+    @Column(name = "email", length = 10, nullable = false)
     private double customerSalarry;
 
     @Type(type = "json")
     @Column(name = "contact_number", columnDefinition = "json", unique = true)
     private ArrayList contactNumber;
 
+
     @Column(name = "active_state", columnDefinition = "TINYINT default 1")
     private boolean activeState;
+
+    @Column(name = "password", columnDefinition = "TINYINT default 1")
+    private boolean password;
+
+    @Column(name = "salt", columnDefinition = "TINYINT default 1")
+    private boolean salt;
+
+    @Column(name = "address", columnDefinition = "TINYINT default 1")
+    private boolean address;
+
 
     public User(String customerName, double customerSalarry, ArrayList contactNumber, boolean activeState) {
         this.customerName = customerName;
