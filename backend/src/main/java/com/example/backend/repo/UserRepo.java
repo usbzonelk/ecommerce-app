8d8a15502517798a515aa9b8171436d38e92b373
chamith <erandachamith322@gmail.com>
2023-03-04 15:13:03 +0530

    enabling jpa repository by using EnableJpaRepositories anotation

--- backend/src/main/java/com/example/backend/repo/UserRepo.java
+++ backend/src/main/java/com/example/backend/repo/UserRepo.java
@@ -2,6 +2,10 @@ package com.example.backend.repo;
 
 import com.example.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
 
+@EnableJpaRepositories
+@Repository
 public interface UserRepo extends JpaRepository<User, Integer> {
 }
