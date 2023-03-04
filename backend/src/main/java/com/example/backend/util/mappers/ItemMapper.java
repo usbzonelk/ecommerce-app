fb4470f83a22e39c7fdb75b3069f6be681229f79
chamith <erandachamith322@gmail.com>
2023-03-04 15:39:55 +0530

    create ItemMapper interface and implement 'DTOtoEntity' method signature

---
+++ backend/src/main/java/com/example/backend/util/mappers/ItemMapper.java
@@ -0,0 +1,14 @@
+package com.example.backend.util.mappers;
+
+import com.example.backend.DTO.ResponseDTO.ItemDTO;
+import com.example.backend.entity.Item;
+import org.mapstruct.Mapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+
+import java.util.List;
+@Mapper(componentModel = "spring")
+public interface ItemMapper {
+    List<ItemDTO> DTOtoEntity(List<Item> items);
+}
