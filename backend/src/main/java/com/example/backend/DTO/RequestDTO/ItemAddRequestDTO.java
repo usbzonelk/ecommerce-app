585dc365003b2a76810c4e6a41a984ae083ce799
chamith <erandachamith322@gmail.com>
2023-03-07 20:29:26 +0530

    create 'ItemRequestDTO'

---
+++ backend/src/main/java/com/example/backend/DTO/RequestDTO/ItemAddRequestDTO.java
@@ -0,0 +1,24 @@
+package com.example.backend.DTO.RequestDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItemAddRequestDTO {
+    private String description;
+    private double unitPrice;
+    private String disPrecentage;
+    private double disPrice;
+    private String availability;
+    private ArrayList images;
+    private String processor;
+    private String brand;
+    private String ssd;
+    private String ram;
+    private String screenSize;
+}
