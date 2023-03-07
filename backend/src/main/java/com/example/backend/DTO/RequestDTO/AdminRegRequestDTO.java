d806777a4a16c47feb56e89674e03189e6fda736
chamith <erandachamith322@gmail.com>
2023-03-07 20:28:46 +0530

    create 'AdminRegRequestDTO'

---
+++ backend/src/main/java/com/example/backend/DTO/RequestDTO/AdminRegRequestDTO.java
@@ -0,0 +1,20 @@
+package com.example.backend.DTO.RequestDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AdminRegRequestDTO {
+    private String adminName;
+    private String email;
+    private ArrayList contactNumber;
+    private boolean activeState;
+    private String  password;
+    private String salt;
+    private String address;
+}
