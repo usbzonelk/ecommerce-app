bc888cbbc55be3820105be27132ed02c14383b7a
chamith <erandachamith322@gmail.com>
2023-03-07 19:48:55 +0530

    add 'addItem()' method signature

--- backend/src/main/java/com/example/backend/service/AdminService.java
+++ backend/src/main/java/com/example/backend/service/AdminService.java
@@ -2,7 +2,10 @@ package com.example.backend.service;
 
 import com.example.backend.DTO.AdminDTO;
 import com.example.backend.DTO.RequestDTO.AdminRegRequestDTO;
+import com.example.backend.DTO.RequestDTO.ItemAddRequestDTO;
 
 public interface AdminService {
     String addAdmin(AdminRegRequestDTO adminRegRequestDTO);
+
+    String addItem(ItemAddRequestDTO itemAddRequestDTO);
 }
