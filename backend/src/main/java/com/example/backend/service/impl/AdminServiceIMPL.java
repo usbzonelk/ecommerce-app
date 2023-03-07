cd28043818c74ab696341d72d3c70c7961cb863a
chamith <erandachamith322@gmail.com>
2023-03-07 20:00:01 +0530

    implement 'addItem()' method .it check item is already exist in the database if it is not exist item add to the database

--- backend/src/main/java/com/example/backend/service/impl/AdminServiceIMPL.java
+++ backend/src/main/java/com/example/backend/service/impl/AdminServiceIMPL.java
@@ -1,6 +1,9 @@
 package com.example.backend.service.impl;
 import com.example.backend.DTO.RequestDTO.AdminRegRequestDTO;
+import com.example.backend.DTO.RequestDTO.ItemAddRequestDTO;
+import com.example.backend.entity.Item;
 import com.example.backend.repo.AdminRepo;
+import com.example.backend.repo.ItemRepo;
 import com.example.backend.service.AdminService;
 import com.example.backend.entity.Admin;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +13,9 @@ import org.springframework.stereotype.Service;
 public class AdminServiceIMPL implements AdminService {
     @Autowired
     private AdminRepo adminRepo;
+
+    @Autowired
+    private ItemRepo itemRepo;
 
     @Override
     public String addAdmin(AdminRegRequestDTO adminRegRequestDTO) {
@@ -28,5 +34,26 @@ public class AdminServiceIMPL implements AdminService {
         }else{
             return admin.getAdminName()+" is already registered " ;
         }
+    }
+    @Override
+    public String addItem(ItemAddRequestDTO itemAddRequestDTO) {
+        Item item = new Item(
+                itemAddRequestDTO.getDescription(),
+                itemAddRequestDTO.getUnitPrice(),
+                itemAddRequestDTO.getDisPrecentage(),
+                itemAddRequestDTO.getDisPrice(),
+                itemAddRequestDTO.getAvailability(),
+                itemAddRequestDTO.getImages(),
+                itemAddRequestDTO.getProcessor(),
+                itemAddRequestDTO.getBrand(),
+                itemAddRequestDTO.getSsd(),
+                itemAddRequestDTO.getRam(),
+                itemAddRequestDTO.getScreenSize()
+        );
+        if(!itemRepo.existsById(item.getItemID())){
+            itemRepo.save(item);
+            return "id = "+item.getItemID()+" item is added ";
+        }
+        return  "id = "+item.getItemID()+" item is already added ";
     }
 }
