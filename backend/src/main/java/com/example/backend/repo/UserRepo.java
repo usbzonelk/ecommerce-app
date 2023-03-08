9ce16376d1b677b0b0dfc99c5f2e2b50ac4f8ef3
chamith <erandachamith322@gmail.com>
2023-03-08 09:12:14 +0530

    change 'existByName' method to 'existByEmail'

--- backend/src/main/java/com/example/backend/repo/UserRepo.java
+++ backend/src/main/java/com/example/backend/repo/UserRepo.java
@@ -2,10 +2,17 @@ package com.example.backend.repo;
 
 import com.example.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @EnableJpaRepositories
 @Repository
 public interface UserRepo extends JpaRepository<User, Integer> {
+    @Query(value = "select salt from user",nativeQuery = true)
+    List<String> getAllCustomerSalts();
+    boolean existsByEmail(String email);
+    User findByEmail(String email);
 }
