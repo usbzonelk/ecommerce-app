3127d13b9a002fb7102829c1379f5d7baf2060f5
chamith <erandachamith322@gmail.com>
2023-03-08 19:26:32 +0530

    add 'addAdmin()' method and chnage it names to 'signUp()'

--- backend/src/main/java/com/example/backend/controller/SignUPController.java
+++ backend/src/main/java/com/example/backend/controller/SignUPController.java
@@ -1,5 +1,6 @@
 package com.example.backend.controller;
 
+import com.example.backend.DTO.RequestDTO.AdminRegRequestDTO;
 import com.example.backend.DTO.RequestDTO.UserRegRequestDTO;
 import com.example.backend.service.SignUpService;
 import com.example.backend.util.StandardResponse;
@@ -14,7 +15,8 @@ import org.springframework.web.bind.annotation.*;
 public class SignUPController {
     @Autowired
     private SignUpService signUpService;
-    @PutMapping(path = "/sign-user")
+
+    @PutMapping(path = "/signUp-user")
     public ResponseEntity<StandardResponse> signUP(@RequestBody UserRegRequestDTO userRegRequestDTO){
         String text = signUpService.regUser(userRegRequestDTO);
         return new ResponseEntity<StandardResponse>(
@@ -22,6 +24,18 @@ public class SignUPController {
                         (
                                 201,
                                 "LOOK Sign Up / Sign In state  !!",
+                                text
+                        ), HttpStatus.CREATED);
+    }
+
+    @PutMapping(path = "/signUp-admin")
+    public ResponseEntity<StandardResponse> signUp(@RequestBody AdminRegRequestDTO adminRegRequestDTO){
+        String text = signUpService.addAdmin(adminRegRequestDTO);
+        return new ResponseEntity<StandardResponse>(
+                new StandardResponse
+                        (
+                                201,
+                                "Saved successfully !!",
                                 text
                         ), HttpStatus.CREATED);
     }
