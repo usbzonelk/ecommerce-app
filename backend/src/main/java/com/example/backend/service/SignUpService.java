b1dff161bee0bd236a64ae022cd4624abd783e7d
chamith <erandachamith322@gmail.com>
2023-03-08 19:29:15 +0530

    create 'addAdmin()' method signature

--- backend/src/main/java/com/example/backend/service/SignUpService.java
+++ backend/src/main/java/com/example/backend/service/SignUpService.java
@@ -1,9 +1,11 @@
 package com.example.backend.service;
 
+import com.example.backend.DTO.RequestDTO.AdminRegRequestDTO;
 import com.example.backend.DTO.RequestDTO.UserRegRequestDTO;
 import org.springframework.stereotype.Service;
 
-@Service
 public interface SignUpService {
     String regUser(UserRegRequestDTO userRegRequestDTO);
+
+    String addAdmin(AdminRegRequestDTO adminRegRequestDTO);
 }
