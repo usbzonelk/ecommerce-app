effe3c59c943e2359df86cefbc74921129a35126
chamith <erandachamith322@gmail.com>
2023-03-08 19:53:01 +0530

    create 'AdminRequestSignInDTO.java' DTO to get admin details

---
+++ backend/src/main/java/com/example/backend/DTO/RequestDTO/AdminRequestSignInDTO.java
@@ -0,0 +1,16 @@
+package com.example.backend.DTO.RequestDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class AdminRequestSignInDTO {
+    private String email;
+    private String  password;
+}
