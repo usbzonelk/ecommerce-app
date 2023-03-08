b4e56033645adf40b7aff12ec46602a1bf3390bc
chamith <erandachamith322@gmail.com>
2023-03-08 19:28:02 +0530

    remove 'addAdmin()' method signature

--- backend/src/main/java/com/example/backend/service/AdminService.java
+++ backend/src/main/java/com/example/backend/service/AdminService.java
@@ -5,7 +5,7 @@ import com.example.backend.DTO.RequestDTO.AdminRegRequestDTO;
 import com.example.backend.DTO.RequestDTO.ItemAddRequestDTO;
 
 public interface AdminService {
-    String addAdmin(AdminRegRequestDTO adminRegRequestDTO);
 
     String addItem(ItemAddRequestDTO itemAddRequestDTO);
+
 }
