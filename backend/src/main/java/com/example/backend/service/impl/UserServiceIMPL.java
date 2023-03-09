2521a42e99336b7e15baebca9d45f9f5de3108fe
chamith <erandachamith322@gmail.com>
2023-03-10 01:50:26 +0530

    implement 'resetPass()' method . in this method check whether current password value and salt value same customer exist or not . if customer exist update the password . otherwise throw NotFoundException

--- backend/src/main/java/com/example/backend/service/impl/UserServiceIMPL.java
+++ backend/src/main/java/com/example/backend/service/impl/UserServiceIMPL.java
@@ -1,4 +1,5 @@
 package com.example.backend.service.impl;
+import com.example.backend.DTO.RequestDTO.UserPasswordResetRequestDTO;
 import com.example.backend.DTO.ResponseDTO.UserResponseDTO;
 import com.example.backend.entity.User;
 import com.example.backend.exception.NotFoundException;
@@ -10,5 +11,25 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceIMPL implements UserService {
+    @Autowired
+    private UserRepo userRepo;
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public String resetPass(UserPasswordResetRequestDTO userPasswordResetRequestDTO) {
+        if(userRepo.existsById(userPasswordResetRequestDTO.getId())){
+            User user = userRepo.getById(userPasswordResetRequestDTO.getId());
+            if(bCryptPasswordEncoder.matches(userPasswordResetRequestDTO.getCurrentPass(),user.getSalt())){
+                String newSalt = bCryptPasswordEncoder.encode(userPasswordResetRequestDTO.getNewPass());
+                userRepo.restPassword(newSalt,userPasswordResetRequestDTO.getId());
+                return "password is reset user id = "+userPasswordResetRequestDTO.getId();
+            }else{
+                return "salt value mismatch !!";
+            }
+        }else{
+            throw new NotFoundException("There is no user for id = "+ userPasswordResetRequestDTO.getId());
+        }
+    }
 }
