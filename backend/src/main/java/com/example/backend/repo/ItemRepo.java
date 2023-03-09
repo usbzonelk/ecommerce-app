8e6e4ec6108cbeb0768fd597c071a89fc6a04be1
chamith <erandachamith322@gmail.com>
2023-03-09 23:15:31 +0530

    create native sql query for update item quantity

--- backend/src/main/java/com/example/backend/repo/ItemRepo.java
+++ backend/src/main/java/com/example/backend/repo/ItemRepo.java
@@ -2,17 +2,23 @@ package com.example.backend.repo;
 
 import com.example.backend.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @EnableJpaRepositories
 @Repository
+@Transactional
 public interface ItemRepo extends JpaRepository<Item,Integer> {
     @Query(value = "SELECT i FROM Item i WHERE i.brand LIKE CONCAT('%', :search, '%') OR i.description LIKE CONCAT('%', :search, '%')")
     List<Item> getItemsBySearch(String search);
+    @Modifying
+    @Query(value = "update item set quantity = ?1 where item_id = ?2",nativeQuery = true)
+    void updateItemQTY(int qty , int id);
 
 }
 
