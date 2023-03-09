cd51fc56ddb241b1e7a37d3a9f2649f129404658
chamith <erandachamith322@gmail.com>
2023-03-09 10:27:30 +0530

    add 'getUserUsingID()' from UserServiceIMPL

--- backend/src/main/java/com/example/backend/service/impl/AdminServiceIMPL.java
+++ backend/src/main/java/com/example/backend/service/impl/AdminServiceIMPL.java
@@ -1,12 +1,17 @@
 package com.example.backend.service.impl;
 import com.example.backend.DTO.RequestDTO.AdminRegRequestDTO;
 import com.example.backend.DTO.RequestDTO.ItemAddRequestDTO;
+import com.example.backend.DTO.ResponseDTO.UserResponseDTO;
 import com.example.backend.entity.Item;
+import com.example.backend.entity.User;
+import com.example.backend.exception.NotFoundException;
 import com.example.backend.repo.AdminRepo;
 import com.example.backend.repo.ItemRepo;
+import com.example.backend.repo.UserRepo;
 import com.example.backend.service.AdminService;
 import com.example.backend.entity.Admin;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,6 +21,9 @@ public class AdminServiceIMPL implements AdminService {
 
     @Autowired
     private ItemRepo itemRepo;
+
+    @Autowired
+    private UserRepo userRepo;
 
     @Override
     public String addItem(ItemAddRequestDTO itemAddRequestDTO) {
@@ -37,5 +45,20 @@ public class AdminServiceIMPL implements AdminService {
             return "id = "+item.getItemID()+" item is added ";
         }
         return  "id = "+item.getItemID()+" item is already added ";
+    }
+
+    @Override
+    public UserResponseDTO getUserUsingID(int id){
+        if(userRepo.existsById(id)){
+            User user = userRepo.getById(id);
+            UserResponseDTO userResponseDTO = new UserResponseDTO(
+                    user.getUserId(),
+                    user.getUserName(),
+                    user.getContactNumber()
+            );
+            return userResponseDTO ;
+        }else{
+            throw new NotFoundException("User is not found id = " + id);
+        }
     }
 }
