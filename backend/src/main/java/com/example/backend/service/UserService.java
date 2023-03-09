f198dbcc62641adc9f1ddcc0bcde992dd1a6cd17
chamith <erandachamith322@gmail.com>
2023-03-10 01:46:58 +0530

    add 'resetPass()' method signature

--- backend/src/main/java/com/example/backend/service/UserService.java
+++ backend/src/main/java/com/example/backend/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.backend.service;
 
+import com.example.backend.DTO.RequestDTO.UserPasswordResetRequestDTO;
 import com.example.backend.DTO.RequestDTO.UserRegRequestDTO;
 import com.example.backend.DTO.ResponseDTO.UserResponseDTO;
 import com.example.backend.DTO.UserDTO;
@@ -8,4 +9,5 @@ import org.springframework.stereotype.Service;
 
 public interface UserService {
 
+    String resetPass(UserPasswordResetRequestDTO userPasswordResetRequestDTO);
 }
