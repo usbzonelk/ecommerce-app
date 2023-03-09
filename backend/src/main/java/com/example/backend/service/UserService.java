dcb6c8a1e523c037a6b8c1de731738963918c1bc
chamith <erandachamith322@gmail.com>
2023-03-09 10:22:38 +0530

    remove 'getUserUsingID()' method signature

--- backend/src/main/java/com/example/backend/service/UserService.java
+++ backend/src/main/java/com/example/backend/service/UserService.java
@@ -6,7 +6,6 @@ import com.example.backend.DTO.UserDTO;
 import javassist.NotFoundException;
 import org.springframework.stereotype.Service;
 
-@Service
 public interface UserService {
-    UserResponseDTO getUserUsingID(int id) throws NotFoundException;
+
 }
