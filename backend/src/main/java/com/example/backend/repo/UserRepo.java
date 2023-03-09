3f900425c619957c89c3cbc13a316de0e85b2ef7
chamith <erandachamith322@gmail.com>
2023-03-10 01:45:46 +0530

    create native sql query for update salt value in data base

--- backend/src/main/java/com/example/backend/repo/UserRepo.java
+++ backend/src/main/java/com/example/backend/repo/UserRepo.java
@@ -1,18 +1,27 @@
 package com.example.backend.repo;
 
+import com.example.backend.DTO.RequestDTO.UserPasswordResetRequestDTO;
 import com.example.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @EnableJpaRepositories
 @Repository
+@Transactional
 public interface UserRepo extends JpaRepository<User, Integer> {
     @Query(value = "select salt from user",nativeQuery = true)
     List<String> getAllCustomerSalts();
     boolean existsByEmail(String email);
     User findByEmail(String email);
+
+    @Modifying
+    @Query(value = "update user set salt = ?1 where user_id = ?2",nativeQuery = true)
+    void restPassword(String salt, int id);
+
 }
