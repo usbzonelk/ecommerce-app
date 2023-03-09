b52c6d8e0bb1597c47c127c8da1a22626f269761
chamith <erandachamith322@gmail.com>
2023-03-09 10:23:17 +0530

    add 'getUserUsingID()' method signature

--- backend/src/main/java/com/example/backend/service/AdminService.java
+++ backend/src/main/java/com/example/backend/service/AdminService.java
@@ -3,9 +3,12 @@ package com.example.backend.service;
 import com.example.backend.DTO.AdminDTO;
 import com.example.backend.DTO.RequestDTO.AdminRegRequestDTO;
 import com.example.backend.DTO.RequestDTO.ItemAddRequestDTO;
+import com.example.backend.DTO.ResponseDTO.UserResponseDTO;
+import javassist.NotFoundException;
 
 public interface AdminService {
 
     String addItem(ItemAddRequestDTO itemAddRequestDTO);
+    UserResponseDTO getUserUsingID(int id) ;
 
 }
