3cab07cdf440ac1facb835205b4f50046decc575
chamith <erandachamith322@gmail.com>
2023-03-10 01:40:39 +0530

    creat 'resetPassword()' method . only if user authorized give him to acess for change password

--- backend/src/main/java/com/example/backend/controller/UserController.java
+++ backend/src/main/java/com/example/backend/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.example.backend.controller;
 
+import com.example.backend.DTO.RequestDTO.UserPasswordResetRequestDTO;
 import com.example.backend.DTO.RequestDTO.UserRegRequestDTO;
 import com.example.backend.DTO.ResponseDTO.UserResponseDTO;
+import com.example.backend.authorization.Authorization;
+import com.example.backend.repo.UserRepo;
 import com.example.backend.service.UserService;
 import com.example.backend.util.StandardResponse;
 import javassist.NotFoundException;
@@ -15,7 +18,27 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "api/v1/user")
 
 public class UserController {
+    @Autowired
+    private UserRepo userRepo;
 
+    @Autowired
+    private UserService userService ;
+
+    @Autowired
+    private Authorization authorization;
+
+    @PutMapping(path = "/reset-pass-user")
+    public ResponseEntity<StandardResponse> resetPassword(@RequestBody UserPasswordResetRequestDTO userPasswordResetRequestDTO, @RequestHeader(value = "Authorization") String authorizationHeader){
+        authorization.authorization(authorizationHeader);
+        String text = userService.resetPass(userPasswordResetRequestDTO);
+        return new ResponseEntity<StandardResponse>(
+                new StandardResponse
+                        (
+                                200,
+                                "item id = " + userPasswordResetRequestDTO.getId() + " update status!",
+                                text
+                        ), HttpStatus.OK);
+    }
 
 
 
