e2988fd92b67c97e9a4e09ee5e57f8fffbefba49
chamith <erandachamith322@gmail.com>
2023-03-09 10:11:28 +0530

    Manually handle NotFoundException

--- backend/src/main/java/com/example/backend/service/impl/UserServiceIMPL.java
+++ backend/src/main/java/com/example/backend/service/impl/UserServiceIMPL.java
@@ -1,19 +1,12 @@
 package com.example.backend.service.impl;
-
-import com.example.backend.DTO.RequestDTO.UserRegRequestDTO;
 import com.example.backend.DTO.ResponseDTO.UserResponseDTO;
-import com.example.backend.DTO.UserDTO;
 import com.example.backend.entity.User;
+import com.example.backend.exception.NotFoundException;
 import com.example.backend.repo.UserRepo;
 import com.example.backend.service.UserService;
-import com.example.backend.util.StandardResponse;
-import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 
 @Service
 public class UserServiceIMPL implements UserService {
@@ -24,7 +17,7 @@ public class UserServiceIMPL implements UserService {
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
-    public UserResponseDTO getUserUsingID(int id) throws NotFoundException {
+    public UserResponseDTO getUserUsingID(int id){
         if(userRepo.existsById(id)){
             User user = userRepo.getById(id);
             UserResponseDTO userResponseDTO = new UserResponseDTO(
