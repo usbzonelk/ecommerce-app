f45ad8d5515dc49601c1476658461742d9631012
chamith <erandachamith322@gmail.com>
2023-03-09 09:51:10 +0530

    when admin try to add item to the database in a controller class check admin are authorized user or not by checking authirization header are exist and it start we mentioned prefix and it is match with database details or not . if above checking pass admin can accesss the database . otherwise return the UnauthorizedException which we manually created

--- backend/src/main/java/com/example/backend/controller/AdminController.java
+++ backend/src/main/java/com/example/backend/controller/AdminController.java
@@ -3,12 +3,17 @@ package com.example.backend.controller;
 import com.example.backend.DTO.AdminDTO;
 import com.example.backend.DTO.RequestDTO.AdminRegRequestDTO;
 import com.example.backend.DTO.RequestDTO.ItemAddRequestDTO;
+import com.example.backend.exception.UnauthorizedException;
 import com.example.backend.service.AdminService;
+import com.example.backend.util.JwtUtils;
 import com.example.backend.util.StandardResponse;
+import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.UnavailableException;
 
 @RestController
 @CrossOrigin
@@ -16,17 +21,26 @@ import org.springframework.web.bind.annotation.*;
 public class AdminController {
   @Autowired
   private AdminService adminService ;
-  @PutMapping(path = "/add-item")
-  public ResponseEntity<StandardResponse> addItems(@RequestBody ItemAddRequestDTO itemAddRequestDTO){
-    String text= adminService.addItem(itemAddRequestDTO);
-    return new ResponseEntity<StandardResponse>(
-            new StandardResponse
-                    (
-                            201,
-                            "Added successfully !!",
-                            text
-                    ), HttpStatus.CREATED);
-  }
 
+  @Autowired
+  private JwtUtils jwtUtils;
+
+  @PutMapping(path = "/add-item")
+  public ResponseEntity<StandardResponse> addItems(@RequestBody ItemAddRequestDTO itemAddRequestDTO , @RequestHeader("Authorization") String authorizationHeader) {
+    if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")){
+       String token = authorizationHeader.substring(7);
+       Jwts.parser().setSigningKey(jwtUtils.secret).parseClaimsJws(token);
+       String text= adminService.addItem(itemAddRequestDTO);
+       return new ResponseEntity<StandardResponse>(
+              new StandardResponse
+                      (
+                              201,
+                              "Added successfully !!",
+                              text
+                      ), HttpStatus.CREATED);
+    }else {
+      throw new UnauthorizedException("Unauthorized accesses!!");
+    }
+  }
 
 }
