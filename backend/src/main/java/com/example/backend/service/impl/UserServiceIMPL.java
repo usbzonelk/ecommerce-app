c2cd873ab89073947528d0459f802e94b1520dcc
chamith <erandachamith322@gmail.com>
2023-03-09 10:26:51 +0530

    move 'getUserUsingID()' to AdminServiceIMPL

--- backend/src/main/java/com/example/backend/service/impl/UserServiceIMPL.java
+++ backend/src/main/java/com/example/backend/service/impl/UserServiceIMPL.java
@@ -10,24 +10,5 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceIMPL implements UserService {
-    @Autowired
-    private UserRepo userRepo;
 
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    @Override
-    public UserResponseDTO getUserUsingID(int id){
-        if(userRepo.existsById(id)){
-            User user = userRepo.getById(id);
-            UserResponseDTO userResponseDTO = new UserResponseDTO(
-                    user.getUserId(),
-                    user.getUserName(),
-                    user.getContactNumber()
-            );
-            return userResponseDTO ;
-        }else{
-            throw new NotFoundException("User is not found id = " + id);
-        }
-    }
 }
