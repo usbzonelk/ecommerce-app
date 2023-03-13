736b08740defaacc7c027cd816b0d918dc33031b
chamith <erandachamith322@gmail.com>
2023-03-13 16:41:01 +0530

    extend runtime exception rather than SQLException class

--- backend/src/main/java/com/example/backend/exception/IntergrityConstraintsViolation.java
+++ backend/src/main/java/com/example/backend/exception/IntergrityConstraintsViolation.java
@@ -3,9 +3,10 @@ package com.example.backend.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.sql.SQLException;
 import java.sql.SQLIntegrityConstraintViolationException;
-@ResponseStatus(value =HttpStatus.BAD_GATEWAY , reason = "occur integrity constraints violation")
-public class IntergrityConstraintsViolation extends SQLIntegrityConstraintViolationException {
+@ResponseStatus(value =HttpStatus.BAD_GATEWAY)
+public class IntergrityConstraintsViolation extends RuntimeException {
     public IntergrityConstraintsViolation(String message){
         super(message);
     }
