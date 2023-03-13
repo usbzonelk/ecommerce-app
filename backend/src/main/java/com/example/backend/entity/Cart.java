7ab9c0b3c60cfe316a943eb70eaf657b635a5365
chamith <erandachamith322@gmail.com>
2023-03-13 14:50:16 +0530

    create entity for cart. in this entity have two foreign key which are 'itemId and userId' i map these relation by using @ManyToOne anotation

---
+++ backend/src/main/java/com/example/backend/entity/Cart.java
@@ -0,0 +1,40 @@
+package com.example.backend.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Cart {
+    @Id
+    @Column(name = "cart_id", length = 40)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int cartID;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "itemId", nullable = false)
+    private Item item;
+
+    @Column(name = "Quantity")
+    private int quantity;
+
+    @Column(name = "UnitPrice")
+    private float unitPrice;
+
+    @Column(name = "DiscountPercentage")
+    private float discountPercentage;
+
+}
+
+
+
