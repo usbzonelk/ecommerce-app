354493eda67efd41d1cd656ac9722e60c9f721bf
chamith <erandachamith322@gmail.com>
2023-03-13 15:07:03 +0530

    add 'addToCart' method signature

--- backend/src/main/java/com/example/backend/service/UserService.java
+++ backend/src/main/java/com/example/backend/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.backend.service;
 
+import com.example.backend.DTO.RequestDTO.AddToCartRequestDTO;
 import com.example.backend.DTO.RequestDTO.UserPasswordResetRequestDTO;
 import com.example.backend.DTO.RequestDTO.UserRegRequestDTO;
 import com.example.backend.DTO.ResponseDTO.UserResponseDTO;
@@ -7,7 +8,12 @@ import com.example.backend.DTO.UserDTO;
 import javassist.NotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+
 public interface UserService {
 
     String resetPass(UserPasswordResetRequestDTO userPasswordResetRequestDTO);
+
+    String addToCart(AddToCartRequestDTO addToCartRequestDTO) throws SQLIntegrityConstraintViolationException;
 }
