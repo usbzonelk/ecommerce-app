48afcca428d8589e3f8047098bec6225f3e776f3
chamith <erandachamith322@gmail.com>
2023-03-22 22:01:16 +0530

    add 'removeAllItemById()' method signature

--- backend/src/main/java/com/example/backend/service/UserService.java
+++ backend/src/main/java/com/example/backend/service/UserService.java
@@ -19,4 +19,6 @@ public interface UserService {
     String addToCart(AddToCartRequestDTO addToCartRequestDTO) throws SQLException;
 
     String removeItemById(int id);
+
+    String removeAllItemById(int id);
 }
