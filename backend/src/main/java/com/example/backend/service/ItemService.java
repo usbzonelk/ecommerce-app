8e808f0b1757aa4a0a682233bdc9e6818baa5648
chamith <erandachamith322@gmail.com>
2023-03-22 22:00:14 +0530

    add throws clause to method signature

--- backend/src/main/java/com/example/backend/service/ItemService.java
+++ backend/src/main/java/com/example/backend/service/ItemService.java
@@ -1,9 +1,10 @@
 package com.example.backend.service;
 
 import com.example.backend.DTO.ResponseDTO.ItemDTO;
+import javassist.NotFoundException;
 
 import java.util.List;
 
 public interface ItemService {
-    List<ItemDTO> getItemsBySearch(String searchString);
+    List<ItemDTO> getItemsBySearch(String searchString) throws NotFoundException;
 }
