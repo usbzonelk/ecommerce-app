48de6508975b6c3ac4fe590e6fde196e5ffa7427
chamith <erandachamith322@gmail.com>
2023-03-22 21:58:13 +0530

    create 'deleteAllOrders()' and 'updateQTY()' native quetyies and remove 'existsByItemItemID' and 'getByUserUserId' methods

--- backend/src/main/java/com/example/backend/repo/CartRepo.java
+++ backend/src/main/java/com/example/backend/repo/CartRepo.java
@@ -2,10 +2,15 @@ package com.example.backend.repo;
 
 import com.example.backend.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 @EnableJpaRepositories
 @Repository
@@ -15,5 +20,14 @@ public interface CartRepo extends JpaRepository<Cart,Integer> {
 
     boolean existsByUserUserId(int userId);
 
-    Cart  getByUserUserId(int id);
+    @Modifying
+    @Query(value = "delete  from cart where user_id = ?1" , nativeQuery = true)
+    void deleteAllOrders(int userId);
+
+    @Modifying
+    @Transactional
+    @Query(value = "Update cart set quantity= ?1 where user_id = ?2 and item_id = ?3",nativeQuery = true)
+    int updateQTY(int newQty , int userID , int itemId);
+
+    List<Cart> getAllByUserIsNotNull();
 }
