07feaa31a5b009ec4257fb28a63277ffa568b590
chamith <erandachamith322@gmail.com>
2023-03-23 21:18:30 +0530

    modify 'removeUSer()' method . restrict this method o A and B admin

--- backend/src/main/java/com/example/backend/controller/AdminController.java
+++ backend/src/main/java/com/example/backend/controller/AdminController.java
@@ -6,6 +6,7 @@ import com.example.backend.DTO.RequestDTO.UserPasswordResetRequestDTO;
 import com.example.backend.DTO.ResponseDTO.UserResponseDTO;
 import com.example.backend.authentication.Authentication;
 import com.example.backend.authentication.ExistRevokedToken;
+import com.example.backend.authorization.AdminPrivilage;
 import com.example.backend.exception.IntergrityConstraintsViolation;
 import com.example.backend.exception.JWTExpireException;
 import com.example.backend.exception.UnauthorizedException;
@@ -36,6 +37,9 @@ public class AdminController {
 
   @Autowired
   private ExistRevokedToken existRevokedToken ;
+
+  @Autowired
+  private AdminPrivilage adminPrivilage ;
 
   @PutMapping(path = "/add-item")
   public ResponseEntity<StandardResponse> addItems(@RequestBody ItemAddRequestDTO itemAddRequestDTO ,
@@ -92,22 +96,36 @@ public class AdminController {
 
         }
     }
-    @DeleteMapping(path = "/delete-user/{id}")
-    public ResponseEntity<StandardResponse> removeUSer(@PathVariable(value = "id")int id ,
+    @DeleteMapping(path = "/delete-user",
+                   params = {"userID","adminID"}
+                  )
+    public ResponseEntity<StandardResponse> removeUSer(@RequestParam(value = "userID")int userid ,
+                                                       @RequestParam(value = "adminID")int adminid ,
                                                        @RequestHeader("Authentication") String authenticationHeader
                                                       ) {
-        if (existRevokedToken.checkToken(authenticationHeader)) {
-            throw new UnauthorizedException("token are deactive");
-        } else {
-            authentication.authentication(authenticationHeader);
-            String text = adminService.deleteUser(id);
+        String adminPrivVal = adminPrivilage.getPrivilleged(adminid);
+        if(adminPrivVal.equals("A")  || adminPrivVal.equals("B")){
+            if (existRevokedToken.checkToken(authenticationHeader)) {
+                throw new UnauthorizedException("token are deactive");
+            } else {
+                authentication.authentication(authenticationHeader);
+                String text = adminService.deleteUser(userid);
+                return new ResponseEntity<StandardResponse>(
+                        new StandardResponse
+                                (
+                                        200,
+                                        "customer id = " + userid + " deleted!",
+                                        text
+                                ), HttpStatus.OK);
+            }
+        }else{
             return new ResponseEntity<StandardResponse>(
                     new StandardResponse
                             (
-                                    200,
-                                    "customer id = " + id + " deleted!",
-                                    text
-                            ), HttpStatus.OK);
+                                    406,
+                                    " not access to delete users for type "+adminPrivVal+" admins",
+                                    "HttpStatus.NOT_ACCEPTABLE"
+                            ), HttpStatus.NOT_ACCEPTABLE);
         }
     }
 
