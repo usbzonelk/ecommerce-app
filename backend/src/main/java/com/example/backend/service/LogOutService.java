c223046c9214644094ff7ae42ee45939dbad36df
chamith <erandachamith322@gmail.com>
2023-03-23 17:02:55 +0530

    add 'logOutAdmin()' method signature

--- backend/src/main/java/com/example/backend/service/LogOutService.java
+++ backend/src/main/java/com/example/backend/service/LogOutService.java
@@ -2,4 +2,6 @@ package com.example.backend.service;
 
 public interface LogOutService {
     String logOutUser(String authenticationHeader);
+
+    String logOutAdmin(String authenticationHeader);
 }
