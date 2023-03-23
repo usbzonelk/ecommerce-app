2a3c12b31972d1748f594fba133383f16958fa2f
chamith <erandachamith322@gmail.com>
2023-03-23 11:19:32 +0530

    create 'restPassword()' query method for update admin password

--- backend/src/main/java/com/example/backend/repo/AdminRepo.java
+++ backend/src/main/java/com/example/backend/repo/AdminRepo.java
@@ -2,8 +2,12 @@ package com.example.backend.repo;
 
 import com.example.backend.entity.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
 
 @EnableJpaRepositories
 @Repository
@@ -12,4 +16,9 @@ public interface AdminRepo extends JpaRepository<Admin,Integer> {
     Admin findByEmail(String email);
 
     boolean existsByEmail(String email);
+
+    @Transactional
+    @Modifying
+    @Query(value = "update admin set salt = ?1 where admin_id = ?2",nativeQuery = true)
+    void restPassword(String salt, int id);
 }
