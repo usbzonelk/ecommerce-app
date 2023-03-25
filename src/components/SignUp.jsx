8283e97cfd15d225fc57947a4e47ff203df3b104
AsankaRukshanRathnayake <88618141+AsankaRukshanRathnayake@users.noreply.github.com>
2023-03-25 23:14:55 +0530

    Added form validation
    1. Added link from Signup page to  signin page.
    2. Added form validations for SIgnUp page

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -2,8 +2,59 @@ import React from 'react'
 import { FaUserAlt, FaKey } from 'react-icons/fa';
 import { MdEmail } from 'react-icons/md'
 import { RiLockPasswordFill } from 'react-icons/ri'
+import { useNavigate } from 'react-router-dom';
+import { useFormik } from 'formik';
+import * as Yup from "yup";
 
 const SignUp = () => {
+    const nav = useNavigate();
+
+    const handleLogin = (event) => {
+        nav('../Login');  
+    }
+
+    const handleSubmit = (event) => {
+        nav('../Login');  
+    }
+
+    const formik = useFormik({
+        initialValues : {
+            fullname : "",
+            email : "",
+            password : "",
+            password_repeat : "",
+            form2Example3c:"",
+        },
+
+        //validate forms
+
+        validationSchema : Yup.object({
+            fullname:Yup.string()
+                .required("Name is required"),
+            email:Yup.string()
+                .required("Email is required")
+                .email("Invalid Email Address"),
+            password:Yup.string()
+                .required("Password is required")
+                .min(8,"Password must be at least 8 characters or more.")
+                .max(16,"Password must be 16 charactres or less."),
+            password_repeat:Yup.string()
+            .required("Password is required")
+            .min(8,"Password must be at least 8 characters or more.")
+            .max(16,"Password must be 16 charactres or less."),
+            form2Example3c:Yup.array()
+            .required("You must agree to the Terms & Conditions"),
+        }),
+
+        onSubmit: (values) => {
+          console.log(values);
+        },
+    });
+
+    console.log(formik.errors);
+
+    
+
     return (
         <section className="signup row d-flex justify-content-center align-items-center h-100">
             <div className="col-lg-12 col-xl-11">
@@ -13,45 +64,94 @@ const SignUp = () => {
                             <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
                                 <p className="text-center h1 fw-bold mb-4 mx-1 mx-md-4">Sign up</p>
 
-                                <form className="mx-1 mx-md-4">
+                                <form onSubmit = {formik.handleSubmit} className="mx-1 mx-md-4">
+                                    
+                                <div className = "error-message">{formik.touched.fullname && formik.errors.fullname? formik.errors.fullname:""}</div>
                                     <div className="d-flex flex-row align-items-center mb-4">
                                         <FaUserAlt className='me-3' />
                                         <div className="form-outline flex-fill mb-0">
-                                            <input type="text" id="fullname" className="form-control" placeholder='Full Name' />
-                                        </div>
+                                            <input 
+                                                type="text" 
+                                                id="fullname" 
+                                                className="form-control" 
+                                                placeholder='Full Name' 
+                                                value={ formik.values.fullname}
+                                                onChange = {formik.handleChange}
+                                                onBlur = {formik.handleBlur}
+                                            />
+                                        </div>    
                                     </div>
-
+                                    
+                                    <div className = "error-message">{formik.touched.email && formik.errors.email ? formik.errors.email:""}</div>
                                     <div className="d-flex flex-row align-items-center mb-4">
                                         <MdEmail className='me-3' />
                                         <div className="form-outline flex-fill mb-0">
-                                            <input type="email" id="email" className="form-control" placeholder='Email' />
+                                            <input 
+                                                type="email" 
+                                                id="email" 
+                                                className="form-control" 
+                                                placeholder='Email' 
+                                                value={ formik.values.email}
+                                                onChange = {formik.handleChange}
+                                                onBlur = {formik.handleBlur}
+                                            />
                                         </div>
                                     </div>
 
+                                    <div className = "error-message">{formik.touched.password && formik.errors.password ? formik.errors.password:""}</div>
                                     <div className="d-flex flex-row align-items-center mb-4">
                                         <RiLockPasswordFill className='me-3' />
                                         <div className="form-outline flex-fill mb-0">
-                                            <input type="password" id="password" className="form-control" placeholder='Password' />
+                                            <input 
+                                                type="password" 
+                                                id="password" 
+                                                className="form-control" 
+                                                placeholder='Password' 
+                                                value={ formik.values.password}
+                                                onChange = {formik.handleChange}
+                                                onBlur={formik.handleBlur}
+                                            />
                                         </div>
                                     </div>
 
+                                    <div className = "error-message">{formik.touched.password_repeat && formik.errors.password_repeat ? formik.errors.password_repeat:""}</div>
                                     <div className="d-flex flex-row align-items-center mb-4">
                                         <FaKey className='me-3' />
                                         <div className="form-outline flex-fill mb-0">
-                                            <input type="password" id="password-repeat" className="form-control" placeholder='Repeat your password' />
+                                            <input 
+                                                type="password" 
+                                                id="password_repeat" 
+                                                className="form-control" 
+                                                placeholder='Retype your password'
+                                                value={ formik.values.password_repeat}
+                                                onChange = {formik.handleChange} 
+                                                onBlur = {formik.handleBlur}
+                                            />
                                         </div>
                                     </div>
 
+                                    <div className = "error-message">{formik.touched.form2Example3c && formik.errors.form2Example3c ? formik.errors.form2Example3c:""}</div>
                                     <div className="form-check d-flex justify-content-center mb-4">
-                                        <input className="form-check-input me-3" type="checkbox" value="" id="form2Example3c" />
+                                        <input 
+                                            className="form-check-input me-3" 
+                                            type="checkbox"
+                                            id="form2Example3c" 
+                                            value={formik.values.form2Example3c}
+                                            onChange = {formik.handleChange}
+                                            onBlur = {formik.handleBlur}
+                                        />
                                         <label className="form-check-label" for="form2Example3">
                                             I agree all statements in <a href="#" className='text-decoration-none'>Terms of service</a>
                                         </label>
                                     </div>
 
                                     <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                                        <button type="button" className="btn btn-primary btn-lg">Register</button>
+                                        <button type="submit" className="btn btn-primary btn-lg" onclick={handleSubmit}>Register</button>
                                     </div>
+
+                                    <br/>
+                                    Already Have An Account? &nbsp;
+                                    <a href="../Login"><u>Sign in</u></a>
                                 </form>
                             </div>
 
