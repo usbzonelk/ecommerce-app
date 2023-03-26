eda4663597f9a0e5e604de6598614afac7382129
chamith <erandachamith322@gmail.com>
2023-03-26 18:04:42 +0530

    modify method for send html content by using MimeMessage rather than SimpleMail

--- backend/src/main/java/com/example/backend/service/impl/EmailSenderServiceIMPL.java
+++ backend/src/main/java/com/example/backend/service/impl/EmailSenderServiceIMPL.java
@@ -1,25 +1,34 @@
 package com.example.backend.service.impl;
 
+import com.example.backend.service.EmailSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
 @Service
-public class EmailSenderService {
+public class EmailSenderServiceIMPL implements EmailSenderService {
     @Autowired
     private JavaMailSender javaMailSender ;
 
     public void sendEmail(String receiver,
                           String subject ,
-                          String body ){
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("erandachamith322@gmail.com");
-        message.setTo(receiver);
-        message.setText(body);
-        message.setSubject(subject);
-
+                          String body
+                         ) throws MessagingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        String url="dummy url"; // TODO: 3/26/2023 need to add exact link  
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(message, true, "UTF-8");
+        mimeMessageHelper.setFrom("erandachamith322@gmail.com");
+        mimeMessageHelper.setTo(receiver);
+        mimeMessageHelper.setSubject(subject);
+        String messageBody = body + "<br><a href=\"" + url + "\">" + url + "</a>";
+        mimeMessageHelper.setText(messageBody, true);
         javaMailSender.send(message);
-        System.out.println("Mail send successfully ...");
+
     }
+
 }
