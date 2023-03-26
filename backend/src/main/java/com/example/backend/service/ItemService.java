2aa7bbab003fdea85cefb6d3dbef8391b1e52b9c
chamith <erandachamith322@gmail.com>
2023-03-27 02:33:25 +0530

    add 'getAllBrandNames()' method signature

--- backend/src/main/java/com/example/backend/service/ItemService.java
+++ backend/src/main/java/com/example/backend/service/ItemService.java
@@ -1,10 +1,14 @@
 package com.example.backend.service;
 
+import com.example.backend.DTO.ResponseDTO.ItemBrandNameResponseDTO;
 import com.example.backend.DTO.ResponseDTO.ItemDTO;
+import com.example.backend.entity.Item;
 import javassist.NotFoundException;
 
 import java.util.List;
 
 public interface ItemService {
     List<ItemDTO> getItemsBySearch(String searchString) throws NotFoundException;
+
+    List<ItemBrandNameResponseDTO> getAllBrandNames();
 }
