b7c2c3189e1b6087c13f9b0f003b333326a8989f
chamith <erandachamith322@gmail.com>
2023-03-26 20:35:51 +0530

    implement 'verificationAdminOTP()' method . in this method return true if user is verified otherwise return false

--- backend/src/main/java/com/example/backend/util/impl/OtpVerification.java
+++ backend/src/main/java/com/example/backend/util/impl/OtpVerification.java
@@ -1,5 +1,6 @@
 package com.example.backend.util.impl;
 
+import com.example.backend.repo.AdminRepo;
 import com.example.backend.repo.UserRepo;
 import com.example.backend.util.OtpVerify;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +11,19 @@ public class OtpVerification implements OtpVerify {
     @Autowired
     private UserRepo userRepo;
 
-    public boolean verificationOTP( int userID){
+    @Autowired
+    private AdminRepo adminRepo ;
+
+    public boolean verificationUserOTP( int userID){
         if(userRepo.getVerifiedState(userID) == 1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public boolean verificationAdminOTP( int adminID){
+        if(adminRepo.getVerifiedState(adminID) == 1){
             return true;
         }else{
             return false;
