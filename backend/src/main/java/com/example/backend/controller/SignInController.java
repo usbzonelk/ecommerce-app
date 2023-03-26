67961036c62d83ee839501ffdbd54f11dfcf7f32
chamith <erandachamith322@gmail.com>
2023-03-26 18:17:11 +0530

    mpdified 'loginUser'

--- backend/src/main/java/com/example/backend/controller/SignInController.java
+++ backend/src/main/java/com/example/backend/controller/SignInController.java
@@ -3,6 +3,7 @@ package com.example.backend.controller;
 import com.example.backend.DTO.RequestDTO.AdminRequestSignInDTO;
 import com.example.backend.DTO.RequestDTO.UserRequestSignInDTO;
 import com.example.backend.service.SignInService;
+import com.example.backend.util.OtpVerify;
 import com.example.backend.util.StandardResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,16 +17,29 @@ public class SignInController {
     @Autowired
     private SignInService signInService;
 
+    @Autowired
+    private OtpVerify otpVerify ;
+
     @PostMapping(path="/user-login")
     public ResponseEntity<StandardResponse> loginUser(@RequestBody UserRequestSignInDTO userRequestSignInDTO){
-        String text = signInService.signInUser(userRequestSignInDTO);
-        return new ResponseEntity<StandardResponse>(
-                new StandardResponse
-                        (
-                                201,
-                                "LOOK Sign Up / Sign In state  !!",
-                                text
-                        ), HttpStatus.CREATED);
+        if(otpVerify.verificationOTP(userRequestSignInDTO.getUserID())){
+            String text = signInService.signInUser(userRequestSignInDTO);
+            return new ResponseEntity<StandardResponse>(
+                    new StandardResponse
+                            (
+                                    201,
+                                    "LOOK Sign Up / Sign In state  !!",
+                                    text
+                            ), HttpStatus.CREATED);
+        }else {
+            return new ResponseEntity<StandardResponse>(
+                    new StandardResponse
+                            (
+                                    406,
+                                    " User is not still verified his account !!",
+                                    " Please verify your account for Sign IN"
+                            ), HttpStatus.NOT_ACCEPTABLE);
+        }
     }
     @PostMapping(path="/admin-login")
     public ResponseEntity<StandardResponse> loginAdmin(@RequestBody AdminRequestSignInDTO adminRequestSignInDTO){
