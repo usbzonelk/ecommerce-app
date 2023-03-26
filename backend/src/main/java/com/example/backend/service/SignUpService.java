2c678567bc396d8ef27f703211a500193ed940b8
chamith <erandachamith322@gmail.com>
2023-03-26 18:28:13 +0530

    add 'userOTPVerfied()' method signature

--- backend/src/main/java/com/example/backend/service/SignUpService.java
+++ backend/src/main/java/com/example/backend/service/SignUpService.java
@@ -4,8 +4,12 @@ import com.example.backend.DTO.RequestDTO.AdminRegRequestDTO;
 import com.example.backend.DTO.RequestDTO.UserRegRequestDTO;
 import org.springframework.stereotype.Service;
 
+import javax.mail.MessagingException;
+
 public interface SignUpService {
-    String regUser(UserRegRequestDTO userRegRequestDTO);
+    String regUser(UserRegRequestDTO userRegRequestDTO) throws MessagingException;
 
     String addAdmin(AdminRegRequestDTO adminRegRequestDTO);
+
+    String userOTPVerfied(int userID, String otp);
 }
