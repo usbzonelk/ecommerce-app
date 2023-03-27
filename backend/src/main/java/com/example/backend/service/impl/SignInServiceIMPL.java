934bab5f531bbcc7d997806f2ea3881f861eceab
chamith <erandachamith322@gmail.com>
2023-03-28 04:44:47 +0530

    when users and admins sign in genarated token move to the token table

--- backend/src/main/java/com/example/backend/service/impl/SignInServiceIMPL.java
+++ backend/src/main/java/com/example/backend/service/impl/SignInServiceIMPL.java
@@ -3,8 +3,10 @@ package com.example.backend.service.impl;
 import com.example.backend.DTO.RequestDTO.AdminRequestSignInDTO;
 import com.example.backend.DTO.RequestDTO.UserRequestSignInDTO;
 import com.example.backend.entity.Admin;
+import com.example.backend.entity.Tokens;
 import com.example.backend.entity.User;
 import com.example.backend.repo.AdminRepo;
+import com.example.backend.repo.TokenRepo;
 import com.example.backend.repo.UserRepo;
 import com.example.backend.service.SignInService;
 import com.example.backend.util.JwtUtils;
@@ -30,6 +32,9 @@ public class SignInServiceIMPL implements SignInService {
     @Autowired
     private OtpVerify otpVerify ;
 
+    @Autowired
+    private TokenRepo tokenRepo ;
+
     @Override
     public String signInUser(UserRequestSignInDTO userRequestSignInDTO) {
         User user = userRepo.findByEmail(userRequestSignInDTO.getEmail());
@@ -37,6 +42,11 @@ public class SignInServiceIMPL implements SignInService {
             if(user!=null && bCryptPasswordEncoder.matches(userRequestSignInDTO.getPassword(), user.getSalt()) ){
                 String token = jwtUtils.genarateJWT(user);
                 userRepo.updateVerifyState(1,userRequestSignInDTO.getUserID());
+                if(tokenRepo.getToken(userRequestSignInDTO.getUserID()) == null){
+                    tokenRepo.insertData(userRequestSignInDTO.getUserID(),"Bearer "+token);
+                }else{
+                    tokenRepo.updateToken("Bearer "+token ,userRequestSignInDTO.getUserID());
+                }
                 return "User login successfully . token = " + token;
             }else{
                 return "user login fail ";
@@ -49,6 +59,11 @@ public class SignInServiceIMPL implements SignInService {
         if(admin!= null && bCryptPasswordEncoder.matches(adminRequestSignInDTO.getPassword(),admin.getSalt())){
             String token = jwtUtils.genarateJWTForAdmin(admin);
             adminRepo.updateVerifyState(1,adminRequestSignInDTO.getAdminID());
+            if(tokenRepo.getToken(adminRequestSignInDTO.getAdminID()) == null){
+                tokenRepo.insertData(adminRequestSignInDTO.getAdminID(),"Bearer "+token);
+            }else{
+                tokenRepo.updateToken("Bearer "+token ,adminRequestSignInDTO.getAdminID());
+            }
             return "Admin login successfully . token = " + token;
         }else{
             return "Admin login fail ";
