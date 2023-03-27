fd811aab41d2145cbbd425aea85e8ffde50bf9fa
chamith <erandachamith322@gmail.com>
2023-03-28 04:43:34 +0530

    modify signOut methods when they are signout token move to revoketoken table

--- backend/src/main/java/com/example/backend/service/impl/LogOutServiceIMPL.java
+++ backend/src/main/java/com/example/backend/service/impl/LogOutServiceIMPL.java
@@ -1,6 +1,7 @@
 package com.example.backend.service.impl;
 
 import com.example.backend.repo.RevokeTokenRepo;
+import com.example.backend.repo.TokenRepo;
 import com.example.backend.repo.UserRepo;
 import com.example.backend.service.LogOutService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,15 +12,20 @@ public class LogOutServiceIMPL implements LogOutService {
     @Autowired
     private RevokeTokenRepo revokeTokenRepo;
 
+    @Autowired
+    private TokenRepo tokenRepo;
+
     @Override
     public String logOutUser(String authenticationHeader) {
         revokeTokenRepo.insertToken(authenticationHeader);
+        tokenRepo.deleteToken(authenticationHeader);
         return "Log Out Successfully!!";
     }
 
     @Override
     public String logOutAdmin(String authenticationHeader) {
         revokeTokenRepo.insertToken(authenticationHeader);
+        tokenRepo.deleteToken(authenticationHeader);
         return "Log Out Successfully!!";
     }
 }
