5eb3dea004610021d2400975e9e7cfab720b8dec
chamith <erandachamith322@gmail.com>
2023-03-27 17:37:21 +0530

    create 'getCheckoutByUserID()' query method for select checkouts

--- backend/src/main/java/com/example/backend/repo/CheckoutRepo.java
+++ backend/src/main/java/com/example/backend/repo/CheckoutRepo.java
@@ -2,10 +2,16 @@ package com.example.backend.repo;
 
 import com.example.backend.entity.Checkout;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @EnableJpaRepositories
 @Repository
 public interface CheckoutRepo extends JpaRepository<Checkout,Integer> {
+
+    @Query(value = "select * from checkout where user_id = ?1" , nativeQuery = true)
+    List<Checkout> getCheckoutByUserID(int userID);
 }
