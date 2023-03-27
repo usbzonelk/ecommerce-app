7a4131d5c8f2890a19f2eda9cf41e4c81509a1ea
chamith <erandachamith322@gmail.com>
2023-03-28 04:06:39 +0530

    implement 'checkToken()' method . this mehtod return false when token not expire and equals to authentication header only , otherwise return true

--- backend/src/main/java/com/example/backend/authentication/impl/ExistRevokedTokenIMPL.java
+++ backend/src/main/java/com/example/backend/authentication/impl/ExistRevokedTokenIMPL.java
@@ -2,7 +2,10 @@ package com.example.backend.authentication.impl;
 
 import com.example.backend.authentication.ExistRevokedToken;
 import com.example.backend.entity.RevokedToken;
+import com.example.backend.exception.UnauthorizedException;
 import com.example.backend.repo.RevokeTokenRepo;
+import com.example.backend.repo.TokenRepo;
+import jdk.nashorn.internal.parser.Token;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +14,16 @@ public class ExistRevokedTokenIMPL implements ExistRevokedToken {
     @Autowired
     private RevokeTokenRepo revokeTokenRepo;
 
-    public boolean checkToken(String token){
+    @Autowired
+    private TokenRepo tokenRepo ;
+
+    public boolean checkToken(String token , int id){
         RevokedToken revokedToken = revokeTokenRepo.getToken(token);
-        if(revokedToken == null){
+        String row = tokenRepo.getToken(id);
+        if(revokedToken==null && token.equals(row)){
             return false;
         }else{
-            return true;
+            return true ;
         }
     }
 }
