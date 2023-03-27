ba36c9b98ea2c039a277daa852e9aac344a9233a
Bhashith Udawaththa <usbzonelk@gmail.com>
2023-03-27 14:32:23 +0530

    resolved Merge conflict

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
-import Shop from './components/Shop'
 import { Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './utils/Context';
 import Protected from './components/Protected';
+import Header from './components/Header';
 import HomePage from './pages/HomePage';
+import Shop from './components/Shop'
+import Contact from './components/Contact';
+import AboutUs from './components/AboutUs';
+import Footer from './components/Footer';
+import UserDashboard from './pages/UserDashboard';
 
 const App = () => {
   return (
@@ -17,13 +22,16 @@ const App = () => {
           <Route path='/login' element={<Login />} />
           <Route path='/signup' element={<SignUp />} />
           <Route path='/shop' element={<Shop />} />
-          <Route path='/' element={<Protected><HomePage /></Protected>} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/aboutus' element={<AboutUs />} />
+          <Route path='/dashboard' element={<Protected><UserDashboard/></Protected>} />
+          <Route path='/' element={<HomePage />} />
         </Routes>
 
         <Footer />
       </div>
     </AuthProvider>
   );
-};
+}
 
 export default App;
