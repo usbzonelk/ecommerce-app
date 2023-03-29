a7dbc74f32649906053d8d52f1ee2d64d24050f1
chamith <erandachamith322@gmail.com>
2023-03-29 15:51:41 +0530

    remove user and admin ID from 'RequestSignINDTO' get id by using their emails

--- backend/src/main/java/com/example/backend/service/impl/SignInServiceIMPL.java
+++ backend/src/main/java/com/example/backend/service/impl/SignInServiceIMPL.java
@@ -9,6 +9,7 @@ import com.example.backend.repo.AdminRepo;
 import com.example.backend.repo.TokenRepo;
 import com.example.backend.repo.UserRepo;
 import com.example.backend.service.SignInService;
+import com.example.backend.util.GetID;
 import com.example.backend.util.JwtUtils;
 import com.example.backend.util.OtpVerify;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,17 +36,21 @@ public class SignInServiceIMPL implements SignInService {
     @Autowired
     private TokenRepo tokenRepo ;
 
+    @Autowired
+    private GetID getID;
+
     @Override
     public String signInUser(UserRequestSignInDTO userRequestSignInDTO) {
         User user = userRepo.findByEmail(userRequestSignInDTO.getEmail());
+        int userID = getID.getUserID(userRequestSignInDTO.getEmail());
         //check whether user send otp and otp that reside in database
             if(user!=null && bCryptPasswordEncoder.matches(userRequestSignInDTO.getPassword(), user.getSalt()) ){
-                String token = jwtUtils.genarateJWT(user);
-                userRepo.updateVerifyState(1,userRequestSignInDTO.getUserID());
-                if(tokenRepo.getToken(userRequestSignInDTO.getUserID()) == null){
-                    tokenRepo.insertData(userRequestSignInDTO.getUserID(),"Bearer "+token);
+                String token = jwtUtils.genarateJWT(user);// generate token
+                userRepo.updateVerifyState(1,userID); // update verify state to 1
+                if(tokenRepo.getToken(userID) == null){
+                    tokenRepo.insertData(userID,"Bearer "+token); // insert token to token table if this user has not token
                 }else{
-                    tokenRepo.updateToken("Bearer "+token ,userRequestSignInDTO.getUserID());
+                    tokenRepo.updateToken("Bearer "+token ,userID);// update new token if token is alredy have for this user
                 }
                 return "User login successfully . token = " + token;
             }else{
@@ -56,13 +61,14 @@ public class SignInServiceIMPL implements SignInService {
     @Override
     public String signInAdmin(AdminRequestSignInDTO adminRequestSignInDTO) {
         Admin admin = adminRepo.findByEmail(adminRequestSignInDTO.getEmail());
+        int adminID = getID.getAdminID(adminRequestSignInDTO.getEmail());
         if(admin!= null && bCryptPasswordEncoder.matches(adminRequestSignInDTO.getPassword(),admin.getSalt())){
             String token = jwtUtils.genarateJWTForAdmin(admin);
-            adminRepo.updateVerifyState(1,adminRequestSignInDTO.getAdminID());
-            if(tokenRepo.getToken(adminRequestSignInDTO.getAdminID()) == null){
-                tokenRepo.insertData(adminRequestSignInDTO.getAdminID(),"Bearer "+token);
+            adminRepo.updateVerifyState(1,adminID);
+            if(tokenRepo.getToken(adminID) == null){
+                tokenRepo.insertData(adminID,"Bearer "+token);
             }else{
-                tokenRepo.updateToken("Bearer "+token ,adminRequestSignInDTO.getAdminID());
+                tokenRepo.updateToken("Bearer "+token ,adminID);
             }
             return "Admin login successfully . token = " + token;
         }else{
