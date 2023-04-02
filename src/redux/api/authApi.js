dbd9246b7316dd0b265041086b413cd742a80b5c
Bhashith Udawaththa <usbzonelk@gmail.com>
2023-04-03 01:48:34 +0530

    Resolved some package issue

--- src/redux/api/authApi.js
+++ src/redux/api/authApi.js
@@ -1,14 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { useCookies } from "react-cookie";
 import { setCredentials, logOut } from "../features/authSlice";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "https://uapik.app/app/api/",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
-    /*     const [cookies] = useCookies(['token']);
-const token = cookies.token;
- */
     const token = getState().auth.access;
     console.log(token);
     if (token) {
