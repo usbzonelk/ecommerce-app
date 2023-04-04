2e0d2bbd810c43fb5076baf1a07f62a4fb97f6fb
ishini Hettiarachchi <91345814+ishinihettiarachchi@users.noreply.github.com>
2023-04-04 21:05:35 +0530

    added OTP verification component

---
+++ src/components/Otp.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import '../styles/otp.scss'
+
+
+
+
+const Otp = () => {
+  return (
+    <div className="otp-container">
+        <h2>Verify Your Account</h2>
+        <p>We emailed you the 6 digit code to xxxx@email.com<br/>
+        Enter the code below to confirm your email address</p>
+
+        <div className="code-container">
+            <input type="number" className="code" placeholder='0' min={0} max={9} required />
+            <input type="number" className="code" placeholder='0' min={0} max={9} required />
+            <input type="number" className="code" placeholder='0' min={0} max={9} required />
+            <input type="number" className="code" placeholder='0' min={0} max={9} required />
+            <input type="number" className="code" placeholder='0' min={0} max={9} required />
+            <input type="number" className="code" placeholder='0' min={0} max={9} required />
+
+        </div>
+
+        <div className="code-button">
+            <input type="submit" className="submit" value={"Verify"}
+             style={{textTransform:"uppercase", fontSize:"1rem" , padding: "30px", borderRadius:"10px"}}/>
+        </div>
+
+        
+    </div>
+  );
+};
+
+export default Otp;
