a5d0b96e1e74fc79a723c208b625ead206d77746
chamith <erandachamith322@gmail.com>
2023-04-08 15:01:54 +0530

    add int id to method signature

--- backend/src/main/java/com/example/backend/service/EmailSenderService.java
+++ backend/src/main/java/com/example/backend/service/EmailSenderService.java
@@ -3,5 +3,5 @@ package com.example.backend.service;
 import javax.mail.MessagingException;
 
 public interface EmailSenderService {
-     void sendEmail(String receiver, String subject , String body ) throws MessagingException;
+     void sendEmail(String receiver, String subject , String body , int id ) throws MessagingException;
 }
