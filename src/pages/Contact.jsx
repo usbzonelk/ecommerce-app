859ecdd999c6d3f9639a4236b884e643537eac3d
santhossundar <santhoshsunthar@gmail.com>
2023-04-08 11:25:39 +0530

    Added minor changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -22,13 +22,34 @@ const Contact = () => {
   return (
     <section className='contact'>
       <h4>Contact Us</h4>
+      
       <form className='contact-form' onSubmit={handleSubmit}>
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          required />
+
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required />
+
         <label htmlFor="message">Message</label>
-        <textarea id="message" name="message" value={formData.message} onChange={handleChange} required></textarea>
+        <textarea
+          id="message"
+          name="message"
+          value={formData.message}
+          onChange={handleChange}
+          required></textarea>
+
         <button type="submit">Send</button>
       </form>
     </section>
