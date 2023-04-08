c3de6f20f2ef6ffd213bafbeda09bfef646df131
chamith <erandachamith322@gmail.com>
2023-04-08 11:37:54 +0530

    add quantity for DTO

--- backend/src/main/java/com/example/backend/DTO/ResponseDTO/ItemDTO.java
+++ backend/src/main/java/com/example/backend/DTO/ResponseDTO/ItemDTO.java
@@ -23,4 +23,21 @@ public class ItemDTO {
     private String ram;
     private String screenSize;
     private String title;
+    private int quantity;
+
+    public ItemDTO(String description, double unitPrice, String disPrecentage, double disPrice, String availability, ArrayList images, String processor, String brand, String ssd, String ram, String screenSize, String title, int quantity) {
+        this.description = description;
+        this.unitPrice = unitPrice;
+        this.disPrecentage = disPrecentage;
+        this.disPrice = disPrice;
+        this.availability = availability;
+        this.images = images;
+        this.processor = processor;
+        this.brand = brand;
+        this.ssd = ssd;
+        this.ram = ram;
+        this.screenSize = screenSize;
+        this.title = title;
+        this.quantity = quantity;
+    }
 }
