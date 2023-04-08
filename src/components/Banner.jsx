5606505cbd983e3183d5af8e3ff50d90b7677dd5
santhossundar <santhoshsunthar@gmail.com>
2023-04-08 13:27:01 +0530

    Added minor style changes

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import BannerImg from '../images/banner-img.png';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Banner = () => {
     return (
         <div className="banner">
             <div className="container">
                 <div className="banner-content">
-                    <h2 className="banner-title">Your One-Stop <br/>Shop for Laptops</h2>
+                    <h2 className="banner-title">Your One-Stop <br />Shop for Laptops</h2>
                     <p className="banner-text">
-                    Welcome to our laptop store e-commerce web app! 
-                    Here, you'll find a wide range of laptops to suit 
-                    all your computing needs. Whether you're a student, 
-                    professional, gamer, or simply looking for a reliable 
-                    machine for everyday use, we've got you covered
+                        Welcome to our laptop store e-commerce web app!
+                        Here, you'll find a wide range of laptops to suit
+                        all your computing needs. Whether you're a student,
+                        professional, gamer, or simply looking for a reliable
+                        machine for everyday use, we've got you covered
                     </p>
-                    <Link to="/shop" className="banner-btn">Shop Now</Link>
+
+                    <button  className="banner-btn">
+                        <Link to="/shop">
+                            Shop Now
+                        </Link>
+                    </button>
                 </div>
 
                 <img src={BannerImg} alt="Banner image" className="banner-img" />
